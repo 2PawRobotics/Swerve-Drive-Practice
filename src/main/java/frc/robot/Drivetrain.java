6536a26677ffb112ec20c1b525f5a71ccf9aa21d
2PawRobotics <hsrobotics@ppps.org>
2022-01-31 15:52:28 -0500

    Hello World!
    This is the code that still doesn't work, however we are FINALLY connected to github!

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,115 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard; 
+//import frc.robot.SwerveModule;
+
+//import edu.wpi.first.hal.DIOJNI;
+
+/**                                                                                                    
+ * Represents a swerve drive style drivetrain.                                                                
+ */
+public class Drivetrain {
+  public static final double kMaxSpeed = 3.0; // 3 meters per second
+  public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
+
+  private final Translation2d m_frontLeftLocation = new Translation2d(0.23495, 0.24765);     
+  private final Translation2d m_frontRightLocation = new Translation2d(0.23495, -0.24765);         
+  private final Translation2d m_backLeftLocation = new Translation2d(-0.23495, 0.24765);            
+  private final Translation2d m_backRightLocation = new Translation2d(-0.23495, -0.24765);    
+                                                                                                
+  private final SwerveModule m_frontLeft = new SwerveModule(1, 2, 2, 3);           
+  private final SwerveModule m_frontRight = new SwerveModule(3, 4, 0, 1);
+  private final SwerveModule m_backLeft = new SwerveModule(5, 6, 4, 5); 
+  private final SwerveModule m_backRight = new SwerveModule(7, 8, 6, 7);
+
+  private  AnalogGyro m_gyro = new AnalogGyro(1);
+                                                                                                  
+  private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+      m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
+  );
+
+  private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics, getAngle());
+
+  public Drivetrain() {
+    m_gyro.reset();
+  }
+
+  public void periodic() {
+    Shuffleboard.getTab("Encoders").addBoolean("Direction",() -> m_frontLeft.getDirection());
+    Shuffleboard.getTab("Encoders").addBoolean("Direction",() -> m_frontRight.getDirection());
+    Shuffleboard.getTab("Encoders").addBoolean("Direction",() -> m_backLeft.getDirection());
+    Shuffleboard.getTab("Encoders").addBoolean("Direction",() -> m_backRight.getDirection());
+
+
+    SmartDashboard.putNumber("Front Left Distance", m_frontLeft.getDistance());
+    SmartDashboard.putNumber("Front Right Distance", m_frontRight.getDistance());
+    SmartDashboard.putNumber("Back Left Distance", m_backLeft.getDistance());            
+    SmartDashboard.putNumber("Back Right Distance", m_backRight.getDistance());
+  
+    SmartDashboard.putBoolean("Front Left Direction", m_frontLeft.getDirection());
+    SmartDashboard.putBoolean("Front Right Direction", m_frontRight.getDirection());
+    SmartDashboard.putBoolean("Back Left Direction", m_backLeft.getDirection());
+    SmartDashboard.putBoolean("Back Right Direction", m_backRight.getDirection());
+
+  }
+
+  /**
+   * Returns the angle of the robot as a Rotation2d.
+   *
+   * @return The angle of the robot.
+   */    
+  public Rotation2d getAngle() {
+    // Negating the angle because WPILib gyros are CW positive.   ?? gyro
+    return Rotation2d.fromDegrees(-m_gyro.getAngle());
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   */
+  @SuppressWarnings("ParameterName")
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+    var swerveModuleStates = m_kinematics.toSwerveModuleStates(
+        fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
+            xSpeed, ySpeed, rot, getAngle())
+            : new ChassisSpeeds(xSpeed, ySpeed, rot)
+    );
+    
+    SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates, kMaxSpeed);
+    m_frontLeft.setDesiredState(swerveModuleStates[0]);
+    m_frontRight.setDesiredState(swerveModuleStates[1]);
+    m_backLeft.setDesiredState(swerveModuleStates[2]);
+    m_backRight.setDesiredState(swerveModuleStates[3]);
+  }
+
+  /**
+   * Updates the field relative position of the robot.
+   */
+  public void updateOdometry() {
+    m_odometry.update(
+        getAngle(),
+        m_frontLeft.getState(),
+        m_frontRight.getState(),
+        m_backLeft.getState(),
+        m_backRight.getState()
+    );
+  }
+}
---
+++ src/main/java/frc/robot/SwerveModule.java
@@ -0,0 +1,141 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+//import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX; 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+//import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+
+
+public class SwerveModule {
+  private static final double kWheelRadius = 0.508;
+  private static final int kEncoderResolution = 4096;
+  private static final int kEncoderResolution2 = 2048;
+
+  private static final double kModuleMaxAngularVelocity = Drivetrain.kMaxAngularSpeed;
+  private static final double kModuleMaxAngularAcceleration
+      = 2 * Math.PI; // radians per second squared
+
+  private final WPI_TalonFX m_driveMotor;              //was speedcontroller
+  private final CANSparkMax m_turningMotor;
+
+/*private final Encoder m_driveEncoder = new Encoder(0, 1 ); ignore these, was from original example
+  private final Encoder m_turningEncoder = new Encoder(2, 3);  */
+  
+  public static Encoder m_magEncoder; 
+  public static WPI_TalonFX m_driveEncoder;           ///2048 resolution
+  
+  private final PIDController m_drivePIDController = new PIDController(0.1, 0.01, 0.087);    
+
+  //KP initialy set to 1
+  private final ProfiledPIDController m_turningPIDController
+      = new ProfiledPIDController(1, 1, 0,
+      new TrapezoidProfile.Constraints(kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration));  //was 1,0,0
+
+  // Gains are for example purposes only - must be determined for your own robot!
+  private final SimpleMotorFeedforward m_driveFeedforward = new SimpleMotorFeedforward(1, 3);    //1,3
+  private final SimpleMotorFeedforward m_turnFeedforward = new SimpleMotorFeedforward(4, .5);     //1,.5
+//                                                              first param controls speed of z , second param mystery       
+  /**
+   * Constructs a SwerveModule.
+   *
+   * @param driveMotorChannel   ID for the drive motor.
+   * @param turningMotorChannel ID for the turning motor.
+   */
+  public SwerveModule(int driveMotorChannel, int turningMotorChannel, int aChannel, int bChannel) {
+    m_driveMotor = new WPI_TalonFX(driveMotorChannel);
+    m_turningMotor = new CANSparkMax(turningMotorChannel, MotorType.kBrushed);
+    
+    m_magEncoder = new Encoder (aChannel, bChannel);  
+    m_driveEncoder = new WPI_TalonFX(0);
+
+    //                                               enabled | Limit(amp) | Trigger Threshold(amp) | Trigger Threshold Time(s) 
+    m_driveMotor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 60, 65, 0.25));
+
+    m_driveMotor.set(m_drivePIDController.calculate(m_driveEncoder.get(), .5));
+    m_turningMotor.set(m_turningPIDController.calculate(m_magEncoder.getDistance(), .5));
+
+    // Set the distance per pulse for the drive encoder. We can simply use the
+    // distance traveled for one rotation of the wheel divided by the encoder
+    // resolution.
+    //m_magEncoder.setDistancePerPulse(2 * Math.PI * kWheelRadius / kEncoderResolution); //was driveencoder
+    m_driveEncoder.set(2 * Math.PI * kWheelRadius / kEncoderResolution2);
+
+    // Set the distance (in this case, angle) per pulse for the turning encoder.
+    // This is the the angle through an entire rotation (2 * wpi::math::pi)                                   mallory did it
+    // divided by the encoder resolution.
+    m_magEncoder.setDistancePerPulse(2 * Math.PI / kEncoderResolution); // was turn encoder
+
+    // Limit the PID Controller's input range between -pi and pi and set the input
+    // to be continuous.
+    m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+  }
+ 
+  public void periodic() throws InterruptedException{
+    m_magEncoder.wait(500);
+  }
+
+  public boolean getDirection(){
+    return m_magEncoder.getDirection();
+  }
+
+  public double getDistance(){
+    return m_magEncoder.getDistance();
+  }
+
+  public void reset(){
+    m_magEncoder.reset();
+  }
+
+  /**
+   * Returns the current state of the module.
+   *
+   * @return The current state of the module.
+   */
+  public SwerveModuleState getState() {
+    //first param was mag encoder.getrate()
+    return new SwerveModuleState(m_driveEncoder.getSelectedSensorVelocity(), new Rotation2d(m_magEncoder.get())); //was drive then turn encoder
+  }
+
+  /**
+   * Sets the desired state for the module.
+   *
+   * @param state Desired state with speed and angle.
+   */
+  public void setDesiredState(SwerveModuleState state) {
+    // Calculate the drive output from the drive PID controller. 
+    
+
+    final double driveOutput = m_drivePIDController.calculate(
+        m_driveEncoder.getSelectedSensorVelocity(), state.speedMetersPerSecond); // was drive encoder, as mag
+                     //was get rate
+    final double driveFeedforward = m_driveFeedforward.calculate(state.speedMetersPerSecond);
+
+    // Calculate the turning motor output from the turning PID controller.
+    final double turnOutput = m_turningPIDController.calculate(
+        m_magEncoder.get(), state.angle.getRadians()   // was turn encoder
+    );
+
+    final double turnFeedforward =
+        m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity);
+
+    m_driveMotor.setVoltage(driveOutput + driveFeedforward);
+    m_turningMotor.setVoltage(turnOutput + turnFeedforward);
+  }
+}
